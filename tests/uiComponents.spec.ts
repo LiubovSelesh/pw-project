3b80e94058bed25001c950f7ad76072b85312e53
Liubov.Selesh <seleshbilousl@gmail.com>
2025-07-18 15:24:18 +0200

    added new tests 'radio buttons', use the toBeFalsy & toBeTruthy

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -13,7 +13,7 @@ test.describe('Form Layouts page', () => {
         await page.getByText('Form Layouts').click()       
     })
     test('input fields', async({page}) => {
-        const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Gri"}).getByRole('textbox', {name: "Email"})
+        const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"})
 
         await usingTheGridEmailInput.fill('test@test.com')
         await usingTheGridEmailInput.clear()
@@ -28,5 +28,18 @@ test.describe('Form Layouts page', () => {
 
     })
 
+    test('radio buttons', async({page}) => {
+        const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+
+        // await usingTheGridForm.getByLabel('Option 1').check({force: true})
+        await usingTheGridForm.getByRole('radio', {name: "Option 1"}).check({force: true})
+        const radioStatus = await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked()
+        expect(radioStatus).toBeTruthy()
+        await expect(usingTheGridForm.getByRole('radio', {name: "Option 1"})).toBeChecked()
+
+        await usingTheGridForm.getByRole('radio', {name: "Option 2"}).check({force: true})
+        expect(await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked()).toBeFalsy  // uselected
+        expect(await usingTheGridForm.getByRole('radio', {name: "Option 2"}).isChecked()).toBeTruthy  // selected
+    })
 
 })
